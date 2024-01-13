eed5fb1c8103e4c389e32e8b38d987b631c133e7
Gabriel Carniel <gpcarniel@outlook.com>
2024-01-13 15:36:13 -0300

    setup with shadcnui

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
+export function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-slate-800 text-slate-100">
+      <Button variant="default">Test</Button>
+    </div>
+  )
+}
