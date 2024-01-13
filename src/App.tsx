1afc1fa616e24eb20e192fcf14ff365d58341ece
Gabriel Carniel <gpcarniel@outlook.com>
2024-01-13 18:40:02 -0300

    config prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Button } from './components/ui/button'
 import './global.css'
+
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-slate-800 text-slate-100">
+    <div className="flex h-screen items-center justify-center bg-slate-800 text-slate-100">
       <Button variant="default">Test</Button>
     </div>
   )
