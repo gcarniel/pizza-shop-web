2e6dd7e2f5794bd0685376cf2c3c189c97cd9191
Gabriel Carniel <gpcarniel@outlook.com>
2024-01-13 20:00:37 -0300

    configuring pages and layout

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+}
