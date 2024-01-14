ba2ea5c09dcb3b5c0400b85ce94f3f0337146fe8
Gabriel Carniel <gpcarniel@outlook.com>
2024-01-14 17:15:24 -0300

    added charts and page 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+
+export function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <img src="404.svg" alt="Pagina não encontrada" className="w-1/3" />
+      <p className="text-accent-foreground">
+        Voltar para o{' '}
+        <Link to="/" className="text-sky-600 dark:text-sky-400">
+          Dashboard
+        </Link>
+      </p>
+    </div>
+  )
+}
